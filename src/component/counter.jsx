9c2a5323075f740453da2fd378bb58d2ff3a6579
David Kumar <david.dn.kumar@gmail.com>
2021-06-12 15:27:21 -0500

    add event handlers

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -6,33 +6,52 @@ class Counter extends Component {
         count:0
     };
 
-    styles = {
-        color: 'black'
+    handleIncrement = () => {
+
+        console.log('button clicked', this);
     }
 
     render() {
 
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
-
         return (
             <div>
-                <span style = {this.styles} className ={classes}>{this.formatCount()}</span>
-                <button className = 'btn btn-secondary btn-sm'>Increment</button>
+                <button onClick = {this.handleIncrement}>Increment </button>
+
             </div>
         );
 
     }
-
-    formatCount(){
-        const {count} = this.state; // destructuring the this.state object to reduce extra of code
-        console.log(count)
-        return count === 0 ? <h1>Zero</h1> : count
-    }
 }
 
 export default Counter;
+
 // only one component can be returned by return function of a render
 // state: object that includes any data that a component would need
 // in between curly braces, we can include any valid JS code
 // jsx expression are just like normal js objects
+// // {} -> passing js object
+
+// RENDERING LISTS
+// jsx is not a templating engine hence it cannot have conditionals
+// {this.state.tags.map(tag => <li key = {tag}>{tag}</li>)} -> rendering array of items into a list
+// To use the map() function, attach it to an array you want to iterate over.
+// The map() function expects a callback as the argument and executes it once for each element in the array
+
+
+// CONDITIONAL RENDERING
+// {this.state.tags.length === 0 && "Please create a new tag."}
+
+// HANDLING EVENTS
+// <button onClick = {this.handleIncrement}>Increment </button>
+// we're not calling the method, we're only passing a reference
+
+// BINDING EVENT HANDLERS
+// 'this' can reference different objects based on how it's called and can sometimes return 'undefined'
+// therefore, we must bind event handlers to 'this'
+// functions in js are objects, hence they have properties and methods
+//     constructor(props){
+//         super(props); // must call constructor of parent class
+//         this.handleIncrement = this.handleIncrement.bind(this); // returns current instance of the Counter method
+//     }
+// arrow functions don't rebind 'this', they inherit it, hence it's better to use it than adding constructor
+// and rebinding everything manually
